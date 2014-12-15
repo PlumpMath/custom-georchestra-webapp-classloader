afde7280a48ab535317f9127ff81b5e593d6d33b
Pierre Mauduit <pierre.mauduit@gmail.com>
2014-12-15 21:03:35 +0100

    Adding classes and stuff

---
+++ src/main/java/org/georchestra/GeorchestraCustomClassLoader.java
@@ -0,0 +1,12 @@
+package org.georchestra;
+
+import org.apache.catalina.loader.WebappClassLoader;
+
+public class GeorchestraCustomClassLoader extends WebappClassLoader {
+
+    protected static final String[] triggers = {
+        "javax.servlet.Servlet",                     // Servlet API
+        "org.geotools.data.ogr.jni.JniOGRDataStoreFactory", // GeoTools OGR
+        "it.geosolutions.imageio.gdalframework.GDALUtilities" // Gdal utilities from imageio-ext
+    };
+}
